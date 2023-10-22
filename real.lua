bc5cc6e773d0bebb40a6d13bf37d6b494752e3fa
Complete <96483406+completez@users.noreply.github.com>
2023-10-22 15:20:32 +0700

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,6 +1,6 @@
 repeat task.wait(.25) until game:IsLoaded()
 
-elseif game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then -- Blox Fruits
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then -- Blox Fruits
     loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/BloxFruit.lua"))()
 elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
     loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/Sakura.lua"))()
