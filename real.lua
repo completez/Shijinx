a9307929d7630900ca56f55eff32e63c50ac75d9
Dektoy <80118554+Pukhet@users.noreply.github.com>
2024-03-16 18:10:17 +0700

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,7 +1,7 @@
 repeat task.wait(.25) until game:IsLoaded()
 
-if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then -- Blox Fruits
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/BloxFruit.lua"))()
+if (game.PlaceId == 15049111150 or game:GetService("Players").LocalPlayer.leaderstats:FindFirstChild("Damage")) then -- Second Piece
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pukhet/Shijin/main/SecondPiece.lua"))()
 elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
     loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/Sakura.lua"))()
 elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Blade Ball") then -- Blade Ball
