0c3720c584ec7cc020a408ffae770ce7beee490c
Complete <96483406+completez@users.noreply.github.com>
2024-03-16 23:05:26 +0700

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,9 +1,10 @@
 repeat task.wait(.25) until game:IsLoaded()
-
-if (game.PlaceId == 15049111150 or game:GetService("Players").LocalPlayer.leaderstats:FindFirstChild("Damage")) then -- Second Piece
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pukhet/Shijin/main/SecondPiece.lua"))()
-elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/Sakura.lua"))()
-elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Blade Ball") then -- Blade Ball
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/BladeBall.lua"))()
-end
+pcall(function()
+        if (game.PlaceId == 15049111150 or game:GetService("Players").LocalPlayer.leaderstats:FindFirstChild("Damage")) then -- Second Piece
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Pukhet/Shijin/main/SecondPiece.lua"))()
+        elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/Sakura.lua"))()
+        elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Blade Ball") then -- Blade Ball
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/BladeBall.lua"))()
+        end
+end)
