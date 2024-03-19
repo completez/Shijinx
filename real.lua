6185cf54d006d613c9475713d396f64b612f401e
Complete <96483406+completez@users.noreply.github.com>
2024-03-19 18:54:44 +0700

    Update real.lua

--- real.lua
+++ real.lua
@@ -2,7 +2,7 @@ repeat task.wait(.25) until game:IsLoaded()
 local Players = game:GetService("Players")
 local MarketplaceService = game:GetService("MarketplaceService")
 
-if game.PlaceId == 15049111150 or (Players.LocalPlayer and Players.LocalPlayer:FindFirstChild("leaderstats") and Players.LocalPlayer.leaderstats:FindFirstChild("Damage")) then -- Second Piece
+if game.PlaceId == 15049111150 or string.find(MarketplaceService:GetProductInfo(game.PlaceId).Name, "Second Piece")) then -- Second Piece
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Pukhet/Shijin/main/SecondPiece.lua"))()
 elseif string.find(MarketplaceService:GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
     loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/Sakura.lua"))()
