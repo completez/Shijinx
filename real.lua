93918dcc8d226c88c740abb0b2de1123f9358596
Complete <96483406+completez@users.noreply.github.com>
2024-03-19 22:16:59 +0700

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,11 +1,9 @@
 repeat task.wait(.25) until game:IsLoaded()
-local Players = game:GetService("Players")
-local MarketplaceService = game:GetService("MarketplaceService")
 
-if game.PlaceId == 15049111150 or string.find(MarketplaceService:GetProductInfo(game.PlaceId).Name, "Second Piece")) then -- Second Piece
+if (game.PlaceId == 15049111150 or string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Second Piece")) then -- Second Piece
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Pukhet/Shijin/main/SecondPiece.lua"))()
-elseif string.find(MarketplaceService:GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
+elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
     loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/Sakura.lua"))()
-elseif string.find(MarketplaceService:GetProductInfo(game.PlaceId).Name, "Blade Ball") then -- Blade Ball
+elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Blade Ball") then -- Blade Ball
     loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/BladeBall.lua"))()
 end
