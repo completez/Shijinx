782331de3403c60a99dc92d15a7ded66d052ddce
Dektoy <80118554+Pukhet@users.noreply.github.com>
2024-03-23 02:39:55 +0700

    Update and rename real.lua to OneX.lua

--- real.lua
+++
@@ -1,9 +0,0 @@
-repeat task.wait(.25) until game:IsLoaded()
-
-if (game.PlaceId == 15049111150 or string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Second Piece")) then -- Second Piece
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pukhet/Shijin/main/SecondPiece.lua"))()
-elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Sakura Stand") then -- Sakura Stand
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/Sakura.lua"))()
-elseif string.find(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Blade Ball") then -- Blade Ball
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/completez/Shijinx/main/BladeBall.lua"))()
-end
